c8db7dbd221ebe8403275459b52a99ea62513267
ABDUR RAHMAN <95411726+abdur8312@users.noreply.github.com>
2023-02-25 12:52:34 +0530

    Update TestJdbc.java

--- src/com/luv2code/jdbc/TestJdbc.java
+++ src/com/luv2code/jdbc/TestJdbc.java
@@ -5,9 +5,6 @@ import java.sql.DriverManager;
 
 public class TestJdbc {
 	public static void main(String[] args) {
-		
-		// Hi I edited it for Github purpose
-		// Nice!!!
 		String jdbcUrl = "jdbc:postgresql://localhost:5432/hb_student_tracker";
 		String user = "hbstudent";
 		String pass = "hbstudent";
